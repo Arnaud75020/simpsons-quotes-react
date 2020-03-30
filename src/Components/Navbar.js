549eec4ae4cecf2832a82d90cdb8dad80faa3ad5
Arnaud Duhé <anro010@hotmail.com>
2020-03-30 17:49:13 +0100

    navbar added but not working

---
+++ src/Components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar = () => {
+    <nav>        
+        <a className="App-link" href="https://en.wikipedia.org/wiki/The_Simpsons" target="_blank">Simpson Quotes</a>
+    </nav>
+  };
+  
+  export default Navbar;
