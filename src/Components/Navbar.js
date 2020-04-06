9ffcd1cae729ea78752c7731a2903bdcfb8066d0
Arnaud Duhé <anro010@hotmail.com>
2020-04-06 18:00:55 +0100

    solution react quest 06

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Navbar.css';
 
-const Navbar = () => {
-    <nav>        
-        <a className="App-link" href="https://en.wikipedia.org/wiki/The_Simpsons" target="_blank">Simpson Quotes</a>
+function Navbar() {
+return (
+    <nav className="Navbar">
+    <a href="https://en.wikipedia.org/wiki/The_Simpsons">Simpons Quotes</a>
     </nav>
-  };
-  
-  export default Navbar;
+);
+}
+
+export default Navbar;
